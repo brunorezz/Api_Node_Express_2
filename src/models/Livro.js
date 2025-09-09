092d88f783fb7221fdfcb055c2f3712b767bd8bd
Bruno <bruno765rez@gmail.com>
2025-09-09 10:04:16 -0300

    Corrigir nomes

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,8 +4,8 @@ const livroSchema = new mongoose.Schema(
   {
     id: { type: String },
     titulo: { type: String, required: [true, "O título do livro é obrigatório!"] },
-    autor: { type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: [true, "O nome do autor é obrigatório!"] },
-    editora: { type: String, required: [true, "O nome da editora é obrigatório!"] },
+    autor: { type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: [true, "Id do autor é obrigatório!"] },
+    editora: { type: String, required: [true, "A editora é obrigatório!"] },
     numeroPaginas: { type: Number }
   }
 );

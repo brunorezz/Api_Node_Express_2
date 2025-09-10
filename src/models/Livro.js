de47244d13648c1760c5ea86300c83ea4a35c049
Bruno <bruno765rez@gmail.com>
2025-09-10 09:01:44 -0300

    Adionar validação personalizada

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -6,7 +6,14 @@ const livroSchema = new mongoose.Schema(
     titulo: { type: String, required: [true, "O título do livro é obrigatório!"] },
     autor: { type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: [true, "Id do autor é obrigatório!"] },
     editora: { type: String, required: [true, "A editora é obrigatório!"] },
-    numeroPaginas: { type: Number }
+    paginas:
+    {
+      type: Number, validate: {
+        validator: (valor) => {
+          return valor >= 1 && valor <= 5000;
+        }, message: "O número de páginas deve estar entre 1 e 5000."
+      },
+    }
   }
 );
 
